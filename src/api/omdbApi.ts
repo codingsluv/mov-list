c511910230aa41a8ca325a606e29f3e2d1262e10
MuhammadNur <delusionboys974@gmail.com>
2025-01-12 21:41:33 +0700

    fix(api): handle errors and return null for searchMovieDetailsById
    - Handle HTTP errors when fetching movie details.
    - Return null in case of errors instead of throwing an exception.

--- src/api/omdbApi.ts
+++ src/api/omdbApi.ts
@@ -1,24 +1,32 @@
 const API_KEY = "e6bcb153";
-const BASE_URL = "http://www.omdbapi.com/";
+const BASE_URL = "http://www.omdbapi.com";
 
 // ? fungsi untuk mencari film berdasarkan judul
 export const searchMovieByTitle = async (query:string) => {
     try {
         const response = await fetch(`${BASE_URL}/?apikey=${API_KEY}&s=${encodeURIComponent(query)}`);
         if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
         return data.Search || []; // Pastikan mengembalikan array
-      } catch (error) {
+    } catch (error) {
         console.error("Error fetching movies:", error);
         return []; // Return empty array in case of error
-      }
+    }
 }
 
 // ? fungsi untuk mencari detail film berdasarkan id
 export const searcDetailhMovieByID = async (id:string) => {
-    const response = await fetch(`${BASE_URL}?apikey=${API_KEY}&i=${id}`);
-    const data = await response.json();
-    return data
-} 
+    try {
+        const response = await fetch(`${BASE_URL}/?apikey=${API_KEY}&i=${id}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching movie details:", error);
+        return null; // Return null in case of error
+    }
+}
